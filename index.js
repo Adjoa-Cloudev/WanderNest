43a2f7bdca240206bb470143ef1e24df4206d527
Your Name <user.name>
2025-04-22 20:40:31 +0000

    New changes added

--- index.js
+++ index.js
@@ -16,12 +16,10 @@ mongoose
   .then(() => console.log('Connected to MongoDB...'))
   .catch((err) => console.error('Could not connect to MongoDB:', err.message));
 
-
-app.use('/api/users', userRoutes);  
-app.use('/api/tours', tourRoutes);  
+app.use('/api/users', userRoutes);
+app.use('/api/tours', tourRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
